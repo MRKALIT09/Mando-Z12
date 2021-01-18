aa9101a4fc6d3085a213ac327a238edd29c9b0c9
MedanXploit12 <75316440+MRKALIT09@users.noreply.github.com>
2021-01-18 13:38:56 +0700

    Add files via upload

---
+++ Newbie.sh
@@ -0,0 +1,170 @@
+#!bin/bash
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+
+
+echo -n "  PASSWORD NYA APA ? TOD => : "
+read pw;
+
+if [ $pw = 'MRKALIT09' ]
+then
+echo $me "[•] LOADING......."
+sleep 8
+echo   "    HALLO BRO SEHAT KAN? "
+sleep 3
+sleep 2
+      figlet "done"
+else
+      echo   $me " [•] LOADING YA TOD ...... "
+      sleep 7
+     echo "       SALAH MAU GUA GAMPAR LU? "
+  exit
+  fi
+
+
+
+echo   $me "                          █████████                                  "
+echo   $me "                          █▄█████▄█ {•}======={•}                    "
+echo   $me "                          █▼▼▼▼▼       DARK FB                       "
+echo   $pu "                          █▲▲▲▲▲    {•}======={•}                    "
+echo   $pu "                          █████████                                  "
+echo   $pu "                          ██ ██                                      "
+sleep 3
+echo  $me "                      ●▬▬▬▬▬▬▬▬●●▬▬▬▬▬▬▬▬●●▬▬▬▬▬▬▬▬●                  "
+echo  $me "                          AUTHOR : MR •KALIT09                        "
+echo  $me "                         MY PARTNER: MR • EMRY                        "
+echo  $pu "                       WHATSAPP ME :082377804293                      "
+echo  $pu "                     https://github.com/MRKALIT09                     "
+echo  $pu "                     ●▬▬▬▬▬▬▬▬●●▬▬▬▬▬▬▬▬●●▬▬▬▬▬▬▬▬●                   "
+#batas gan
+echo  $me "                    ◢▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔◣                  "
+echo  $pu "                    ☬    MASUKAN MENU TOOLS NYA    ☬                  "
+echo  $me "                    ◥▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁◤                  "
+echo  $me "                         [1] ➣➣➣➣ CRACK MASSAL                   "
+echo  $me "                         [2] ➣➣➣➣ CRACK (LOG)          "
+echo  $me "                         [3] ➣➣➣➣ CRACK BYE BRIGADE   "
+echo  $me "                         [4] ➣➣➣➣ EMAILVULN       "
+echo  $me "                         [5] ➣➣➣➣ MBF Vodobooy     "
+echo  $pu "                         [6] ➣➣➣➣ DARK FB-1       "
+echo  $pu "                         [7] ➣➣➣➣ DARK FB-2           "
+echo  $pu "                         [8] ➣➣➣➣ DARK FB-3           "
+echo  $pu "                         [9] ➣➣➣➣ DARK FB-4           "
+echo  $pu "                         [10]➣➣➣➣ DARK FB-5           "
+echo  $pu "                         [00]➣➣➣➣ EXIT TOOLS          "
+#batas gan
+read -p "                           ➣➣➣➣➣➣➣ : " pill
+#batas gan
+if [ $pill = "1" ]
+then
+echo $me "       SEDANG MEMASUKI TOOLS....."
+sleep 5
+git clone https://github.com/MRKALIT09/crackmassal
+cd crackmassal
+python2 Banjir.py
+fi
+#batas gan
+if [ $pill = "2" ]
+then
+echo $me "          HARAP MENUNGGU 20 TAHUN :v "
+sleep 6
+git clone https://github.com/MRKALIT09/HACKERPEMULA
+cd HACKERPEMULA
+python2 HackerPemula.py
+fi
+#batas gan
+if [ $pill = "3" ]
+then
+echo " [•] HARAP TUNGGU YA TOD ....."
+sleep 5
+git clone https://github.com/FR13ND8/Fb-Cracker-v.3
+cd Fb-Cracker-v.3
+python2 crack.py
+fi
+#batas gan
+if [ $pill = "4" ]
+then
+echo $me  "       SABAR YA ORG SABAR DI SYG JANDA AWOKAWOK     "
+sleep 5
+git clone https://github.com/FR13ND8/EmailVuln
+cd EmailVuln
+python2 vuln.py
+fi
+#batas gan
+if [ $pill = "5" ]
+then
+echo [•] " INI ADALAH SCRIP MBF BYE VODOBOOY "
+sleep 5
+git clone https://github.com/voodoboyy/crack
+cd crack
+python2 crack.py
+fi
+#batas gan
+if [ $pill = "6" ]
+then
+echo $me " [•]INI ADALAH SCRIP DARK FB.... "
+sleep 5
+git clone https://github.com/MRKALIT09/Mando09
+cd Mando09
+python2 Mando.py
+fi
+#batas gan
+if [ $pill = "7" ]
+then
+echo $me "        [•] SCRIP DARK-2 "
+sleep 5
+git clone https://github.com/MRKALIT09/HekelTelmos
+cd HekelTelmos
+python2 Tools.py
+fi
+#batas gan
+if [ $pill = "8" ]
+then
+echo " SCRIP DARK-3 "
+sleep 5
+git clone https://github.com/MRKALIT09/BangHekel
+cd BangHekel
+python2 dkfb
+fi
+#batas gan
+if [ $pill = "9" ]
+then
+git clone https://github.com/MrDebo/New-Elite
+cd New-Elite
+python2 dark.py
+fi
+#batas gan
+if [ $pill = "10" ]
+then
+echo $me "INI ADALAH SCRIP DARK MILIK THE MAGIZZ "
+git clone https://github.com/TheMagizz/DarkPremium
+cd DarkPremium
+python2 DarkFB.py
+fi
+#batas gan
+if [ $pill = "00" ]
+then
+echo "       JIKA TERJADI KERUSAKAN TERHADAP SCRIP"
+sleep 6
+echo "       WHATSAPP SAYA ADA DI ATAS MOHON JANGAN DI SPAM"
+sleep 6
+echo "       CHAT JIKA HANYA BERKEPENTINGAN SAJA "
+sleep 6
+echo  $cy "    ◢▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔◣ "
+sleep 3
+echo  $cy "    ✰✰  MY FACEBOOK => Su Mando  ✰✰   "
+sleep 3
+echo  $cy "   ✰✰ FACEBOOK PARTNER => Kang Tarik ✰✰ "
+sleep 3
+echo  $cy "   ✰✰ WHATSAPP ME  => 082377804293 ✰✰   "
+sleep 2
+echo  $cy "    ◥▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁◤  "
+figlet       "DONE"
+fi
+exit
+
